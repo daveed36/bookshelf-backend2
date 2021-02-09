2a7759d4c2c08c8b1c0d7c3933a8aca7dd6606cc
Maher George Brikae <mahergeorgebrikae@Mahers-MacBook-Pro.local>
2021-02-08 23:17:34 -0500

    ab

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,20 +2,11 @@ require 'rest-client'
 
 class ApiController < ApplicationController
 
-  def nyt
-    render json: nyt_data
-  end
-
   def google
     render json: google_data
   end
 
   private
-
-  def nyt_data
-    response = RestClient.get("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=#{ENV['NYT_KEY']}")
-    response.body
-  end
 
   def google_data
     query = params[:query].gsub(/\s/, "+")
